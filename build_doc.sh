f5fb092edc6ab80673b8a4f0db95b879ec8f83e9
Mathias Westerdahl <mwesterdahl76@gmail.com>
2013-02-24 12:02:37 +0100

    Build doc now assembles the doc into a zip

--- build_doc.sh
+++ build_doc.sh
@@ -3,3 +3,6 @@ rm -rf build/
 LD_LIBRARY_PATH=/opt/local/lib:. python ../fontcreator.py -h > source/modules/options.txt
 LD_LIBRARY_PATH=/opt/local/lib:. make html
 popd
+
+rm doc.zip
+zip -rq doc.zip doc/build/html/
